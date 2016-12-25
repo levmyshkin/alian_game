726a88fa860d0440c52c59b65376182ee7476335
Ivan Abramenko <levmyshkin89@gmail.com>
2016-12-25 20:37:26 +0600

    Ship can shoot

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
+
 
 def run_game():
     # Init game and create a window.
@@ -12,12 +14,14 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # Creating a ship.
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     # Start main loop of the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
 

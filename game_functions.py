096b6149e6c451e80944e36fdf6bb19600068722
Ivan Abramenko <levmyshkin89@gmail.com>
2017-01-01 14:27:12 +0600

    Recreate fleet after last alien is killed

--- game_functions.py
+++ game_functions.py
@@ -39,11 +39,19 @@ def check_keyup_events(event, ship):
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
     # Remove old bullets.
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if len(aliens) == 0:
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
